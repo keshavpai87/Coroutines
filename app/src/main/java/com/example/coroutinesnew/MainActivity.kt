ae6679244d8d4c213df9e4133d4ffae936f2280b
Keshav Pai <keshu.pai@gmail.com>
2025-03-18 08:17:35 +0530

    Initial Commit with working code without coroutine added

---
+++ app/src/main/java/com/example/coroutinesnew/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.coroutinesnew
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private var count = 0
+    private lateinit var btnDownloadUserData : Button
+    private lateinit var btnCount : Button
+    private lateinit var tvCount : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        btnDownloadUserData = findViewById(R.id.btnDownloadUserData)
+        btnCount = findViewById(R.id.btnCount)
+        tvCount = findViewById(R.id.tvCount)
+
+        btnCount.setOnClickListener {
+            tvCount.text = count++.toString()
+            Log.i("MyTag", "Button Count ${Thread.currentThread().name}")
+        }
+
+        btnDownloadUserData.setOnClickListener {
+            downloadUserData()
+        }
+    }
+
+    private fun downloadUserData() {
+        for (i in 1..200000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+        }
+    }
+}
